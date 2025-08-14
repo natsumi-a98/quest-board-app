904c5a0d15ca7cc530c319eb50ee02fb47fa1fd1
natsumi-a98 <saminatsumi@gmail.com>
2025-08-14 21:41:38 +0900

    名前登録フォーム追加

--- apps/frontend/src/app/signUp/page.tsx
+++ apps/frontend/src/app/signUp/page.tsx
@@ -5,13 +5,14 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function SignupPage() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
 
   const handleSignup = async () => {
     try {
-      await signUp(email, password);
+      await signUp(name, email, password);
       alert("サインアップ成功");
       router.push("/");
     } catch (error: any) {
@@ -30,6 +31,12 @@ export default function SignupPage() {
         <h1 className="text-2xl font-bold mb-6 text-center text-[#1e3a8a]">
           新規登録
         </h1>
+        <input
+          className="w-full bg-white border border-gray-400 p-3 mb-4 rounded focus:outline-none focus:ring-2 focus:ring-[#fbbf24]"
+          placeholder="名前"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <input
           className="w-full bg-white border border-gray-400 p-3 mb-4 rounded focus:outline-none focus:ring-2 focus:ring-[#fbbf24]"
           placeholder="メールアドレス"
